d8aec0e5676e59cd02d29c50d6746ad9cea2ef58
Hyder29 <gulamhaidersrk17@gmail.com>
2026-04-10 20:33:42 +0530

    Updated Code

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,27 +1,27 @@
-import { Instagram, Mail, Twitter, Youtube } from "lucide-react";
+import { Instagram, Linkedin, Mail, Twitter, Youtube } from "lucide-react";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
       <div className="flex items-center gap-6 text-sm text-gray-400">
-        Made with ❤️ by RoadsideCoder
+        Made with ❤️ Gulam Hyder
       </div>
 
       <div className="flex gap-4 items-center">
         <a
-          href="https://www.youtube.com/roadsidecoder"
+          href="https://linkedin.com/in/gulamhyder29"
           className="text-gray-400"
         >
-          <Youtube />
+          <Linkedin className="w-5 h-5" />
         </a>
         <a
-          href="https://www.instagram.com/roadsidecoder"
+          href="https://www.instagram.com/hyderr_29?igsh=b3o4YTlrZ2EwM3F3"
           className="text-gray-400"
         >
           <Instagram className="w-5 h-5" />
         </a>
-        <a href="https://x.com/Piyush_eon" className="text-gray-400">
+        <a href="https://x.com/hyderr_29" className="text-gray-400">
           <Twitter className="w-5 h-5" />
         </a>
       </div>
